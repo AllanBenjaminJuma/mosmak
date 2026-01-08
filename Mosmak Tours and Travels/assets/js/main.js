0dddd03bcf1ac7b3f2e55b693dd5de7a7126e76f
allan_Ben <allanbenjuma@gmail.com>
2026-01-08 12:59:48 +0300

    Finalised Homepage, Destinations and destination-details

--- Mosmak Tours and Travels/assets/js/main.js
+++ Mosmak Tours and Travels/assets/js/main.js
@@ -177,4 +177,13 @@
     });
   });
 
+  let currentSlide = 0;
+  const slides = document.querySelectorAll('.slides');
+
+  setInterval(() => {
+    slides[currentSlide].classList.remove('active');
+    currentSlide = (currentSlide + 1) % slides.length;
+    slides[currentSlide].classList.add('active');
+  }, 4000);
+
 })();
